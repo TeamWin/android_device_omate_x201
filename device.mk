41e0c46d620af56c06ba30d4cc41fd0a07020b77
Dees Troy <dees_troy@teamw.in>
2014-03-07 13:07:44 +0000

    Initial commit

---
+++ device.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/omate/x201/x201-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/omate/x201/overlay
+
+LOCAL_PATH := device/omate/x201
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_x201
+PRODUCT_DEVICE := x201
